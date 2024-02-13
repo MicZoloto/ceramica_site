9cced702034f0fa8e8e4e816b68c04c04c75436c
MicZoloto <mic.zoloto@gmail.com>
2024-02-13 20:14:08 +0200

    add model product, add urls.py, created superuser, chenged  label

--- ceramica/settings.py
+++ ceramica/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'product.apps.ProductConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'ceramica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- product/models.py
+++ product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('Назва товару', max_length=200, null=True, blank=True)
+    #category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
+    #producer = models.ForeignKey('Producer', on_delete=models.CASCADE, null=True, blank=True)
+    price = models.IntegerField('Ціна товару', null=True, blank=True)
+    image = models.ImageField('Зображення товару ', null=True, blank=True, default='default.png')
+    article = models.CharField(max_length=200, null=True, blank=True)
+    pub_date = models.DateTimeField('Дата публікації', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} / {self.category} / {self.producer}"
+    
+    class Meta:
+        verbose_name = "Виріб"
+        verbose_name_plural = "Вироби"
+    
+    
